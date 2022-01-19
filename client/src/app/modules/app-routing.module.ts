3a36302444eb774fd727fc5aec1a7143ddf7db7f
mymanat <71021952+mymanat@users.noreply.github.com>
2022-01-19 13:26:58 -0500

    page_solo_multijoueur_ajoutee

--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
+import { GameSelectionPageComponent } from '@app/pages/game-selection-page/game-selection-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
 
@@ -9,6 +10,7 @@ const routes: Routes = [
     { path: 'home', component: MainPageComponent },
     { path: 'game', component: GamePageComponent },
     { path: 'material', component: MaterialPageComponent },
+    { path: 'game-selection', component: GameSelectionPageComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
