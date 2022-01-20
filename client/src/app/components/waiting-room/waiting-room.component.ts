7fd279a4ef1592bd4a2b67da0bc46e5faae9eaea
theolouesdon <65035689+theolouesdon@users.noreply.github.com>
2022-01-20 10:35:24 -0500

    Amelioration du fonctionnement du Stepper

--- client/src/app/components/waiting-room/waiting-room.component.ts
+++ client/src/app/components/waiting-room/waiting-room.component.ts
@@ -1,48 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { MatStepper } from '@angular/material/stepper';
 import { GamePreparationPageComponent } from '@app/pages/game-preparation-page/game-preparation-page.component';
-class Player{
-  name: string;
-  constructor(name : string){
-    this.name = name;
-  }
+
+/*
+ *   #TODO : Enelever ces classes une fois déclarés dans les services.
+ */
+class Player {
+    name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
 }
 
-class GameBoard{
-  gameType : string;
-  timeSetting : number;
-  dictionnary : string;
-  player1 : Player;
-  player2 : Player;
+class GameBoard {
+    gameType: string;
+    timeSetting: number;
+    dictionary: string;
+    player1: Player;
+    player2: Player;
 }
 
 @Component({
-  selector: 'app-waiting-room',
-  templateUrl: './waiting-room.component.html',
-  styleUrls: ['./waiting-room.component.scss']
+    selector: 'app-waiting-room',
+    templateUrl: './waiting-room.component.html',
+    styleUrls: ['./waiting-room.component.scss'],
 })
 export class WaitingRoomComponent implements OnInit {
+    @Input() stepper: MatStepper;
+    player1: Player;
+    player2: Player;
+    gameBoard: GameBoard;
+    constructor(private dialogRef: MatDialogRef<GamePreparationPageComponent>) {}
 
-  player1 : Player;
-  player2 : Player;
-  gameBoard : GameBoard;
-  constructor(private dialogRef: MatDialogRef<GamePreparationPageComponent>) { 
-  }
-
-  ngOnInit(): void {
-    this.gameBoard = {gameType : "Waiting", timeSetting : 90, dictionnary: "Dictionnaire Français", player1: this.player1, player2: this.player2};
-    this.player1 = {name:"Jackie"};
-  }
-  
-  set setPlayer2(player : Player){
-    if(player.name && !this.player2){
-      this.player2 = player;
+    ngOnInit(): void {
+        this.gameBoard = { gameType: 'Waiting', timeSetting: 90, dictionary: 'Dictionnaire Français', player1: this.player1, player2: this.player2 };
+        this.player1 = { name: 'Jackie' };
     }
-  }
-  closeDialog(){
-    this.dialogRef.close();
-  }
-  playerArrival(): void{
-    this.player2 = {name:"Francis"};
-  }
+
+    set setPlayer2(player: Player) {
+        if (player.name && !this.player2) {
+            this.player2 = player;
+        }
+    }
+    closeDialog() {
+        this.dialogRef.close();
+    }
+    playerArrival(): void {
+        this.player2 = { name: 'Francis' };
+    }
 }
