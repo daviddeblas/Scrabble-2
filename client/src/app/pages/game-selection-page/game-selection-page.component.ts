c30fe767feb799a4d9361e09be5552c8786df048
nico <23260358+nico14916@users.noreply.github.com>
2022-01-22 17:03:47 -0500

    correction lint

--- client/src/app/pages/game-selection-page/game-selection-page.component.ts
+++ client/src/app/pages/game-selection-page/game-selection-page.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-game-selection-page',
-  templateUrl: './game-selection-page.component.html',
-  styleUrls: ['./game-selection-page.component.scss']
+    selector: 'app-game-selection-page',
+    templateUrl: './game-selection-page.component.html',
+    styleUrls: ['./game-selection-page.component.scss'],
 })
-export class GameSelectionPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class GameSelectionPageComponent {}
