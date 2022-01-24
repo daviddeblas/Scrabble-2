dfbd6b38c41693a61884645aa5bd6f77e22e03d0
nico <23260358+nico14916@users.noreply.github.com>
2022-01-24 16:38:57 -0500

    création grille vide de scrabble

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
 import { GameSelectionPageComponent } from './pages/game-selection-page/game-selection-page.component';
+import { BoardComponent } from './components/board/board.component';
 
 /**
  * Main module that is used in main.ts.
@@ -28,6 +29,7 @@ import { GameSelectionPageComponent } from './pages/game-selection-page/game-sel
         PlayAreaComponent,
         SidebarComponent,
         GameSelectionPageComponent,
+        BoardComponent,
     ],
     imports: [AppMaterialModule, AppRoutingModule, BrowserAnimationsModule, BrowserModule, FormsModule, HttpClientModule],
     providers: [],
---
+++ client/src/app/components/board/board.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'app-board',
+    templateUrl: './board.component.html',
+    styleUrls: ['./board.component.scss'],
+})
+export class BoardComponent {}
