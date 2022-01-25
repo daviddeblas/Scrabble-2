e43246dcd63df82eda713fd01cbc1ad4ac248db9
Raphael Tremblay <raphaeltremblay01@gmail.com>
2022-01-25 15:59:35 -0500

    Fix les tests de board.reducer

--- client/src/app/reducers/board.reducer.spec.ts
+++ client/src/app/reducers/board.reducer.spec.ts
@@ -1,13 +1,14 @@
-import { reducer, initialState } from './board.reducer';
+import * as boardActions from '@app/actions/board.actions';
+import { initialState, reducer } from './board.reducer';
 
 describe('Board Reducer', () => {
-  describe('an unknown action', () => {
-    it('should return the previous state', () => {
-      const action = {} as any;
+    describe('an unknown action', () => {
+        it('should return the previous state', () => {
+            const action = boardActions.syncBoardSuccess({ newBoard: [[]] });
 
-      const result = reducer(initialState, action);
+            const result = reducer(initialState, action);
 
-      expect(result).toBe(initialState);
+            expect(result).toBe(initialState);
+        });
     });
-  });
 });
