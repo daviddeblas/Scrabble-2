3b6118a1d434f8bb8297d74a2f4f1240672c9347
Raphael Tremblay <raphaeltremblay01@gmail.com>
2022-01-25 16:26:38 -0500

    Fix action tests

--- client/src/app/actions/board.actions.spec.ts
+++ client/src/app/actions/board.actions.spec.ts
@@ -1,7 +1,10 @@
+import { Direction } from '@app/classes/word';
 import * as fromBoard from './board.actions';
 
 describe('loadBoards', () => {
-  it('should return an action', () => {
-    expect(fromBoard.loadBoards().type).toBe('[Board] Load Boards');
-  });
+    it('should return an action', () => {
+        expect(fromBoard.addWord({ word: { letters: ['A', 'L', 'L', 'O'], position: { x: 0, y: 0 }, direction: Direction.VERTICAL } }).type).toBe(
+            '[Board] Place New Word',
+        );
+    });
 });
--- client/src/app/actions/player.actions.spec.ts
+++ client/src/app/actions/player.actions.spec.ts
@@ -1,7 +1,7 @@
 import * as fromPlayer from './player.actions';
 
 describe('loadPlayers', () => {
-  it('should return an action', () => {
-    expect(fromPlayer.loadPlayers().type).toBe('[Player] Load Players');
-  });
+    it('should return an action', () => {
+        expect(fromPlayer.loadPlayers({ players: {} }).type).toBe('[Player] Load Players');
+    });
 });
