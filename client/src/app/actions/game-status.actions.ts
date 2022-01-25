00fa84deabb0b843c7919914af06fda02b6e7d48
Raphael Tremblay <raphaeltremblay01@gmail.com>
2022-01-25 16:01:26 -0500

    Oublie de refactor de GameStatus

--- client/src/app/actions/game-status.actions.ts
+++ client/src/app/actions/game-status.actions.ts
@@ -2,19 +2,19 @@ import { Player } from '@app/classes/player';
 import { Word } from '@app/classes/word';
 import { createAction, props } from '@ngrx/store';
 
-export const startGame = createAction('[Game] Start Game', props<{ player: Player }>());
+export const startGame = createAction('[Game Status] Start Game', props<{ player: Player }>());
 
 export const startGameSuccess = createAction(
-    '[Game] Start Game Success',
+    '[Game Status] Start Game Success',
     props<{ currentPlayer: Player; opponentPlayer: Player; activePlayer: string }>(),
 );
 
-export const startGameFailure = createAction('[Game] Start Game Failure', props<{ error: unknown }>());
+export const startGameFailure = createAction('[Game Status] Start Game Failure', props<{ error: unknown }>());
 
-export const startNewRound = createAction('[Game] Start New Round', props<{ activePlayer: string }>());
+export const startNewRound = createAction('[Game Status] Start New Round', props<{ activePlayer: string }>());
 
-export const placeWord = createAction('[Game] Place a Word', props<{ word: Word }>());
+export const placeWord = createAction('[Game Status] Place a Word', props<{ word: Word }>());
 
-export const placeWordSuccess = createAction('[Game] Place a Word Success', props<{ word: Word }>());
+export const placeWordSuccess = createAction('[Game Status] Place a Word Success', props<{ word: Word }>());
 
-export const placeWordFailure = createAction('[Game] Place a Word Failure', props<{ error: unknown }>());
+export const placeWordFailure = createAction('[Game Status] Place a Word Failure', props<{ error: unknown }>());
