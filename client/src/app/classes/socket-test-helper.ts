b0526420457e8755eb599a85f96811001b0d9a94
theolouesdon <65035689+theolouesdon@users.noreply.github.com>
2022-01-25 22:30:42 -0500

    Ajout classe pour tester les action de sockets

---
+++ client/src/app/classes/socket-test-helper.ts
@@ -0,0 +1,27 @@
+type CallbackSignature = (params: unknown) => unknown;
+
+export class SocketTestHelper {
+    private callbacks = new Map<string, CallbackSignature[]>();
+    on(event: string, callback: CallbackSignature): void {
+        if (!this.callbacks.has(event)) {
+            this.callbacks.set(event, []);
+        }
+
+        this.callbacks.get(event)?.push(callback);
+    }
+
+    disconnect(): void {
+        return;
+    }
+
+    peerSideEmit(event: string, params?: unknown) {
+        if (!this.callbacks.has(event)) {
+            return;
+        }
+
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        for (const callback of this.callbacks.get(event)!) {
+            callback(params);
+        }
+    }
+}
