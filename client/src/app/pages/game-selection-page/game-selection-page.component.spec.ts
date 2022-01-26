859f7d6a9f2ecdef18ce0c50c85ddb5936d42ac8
unknown <daviddeblas@ymail.com>
2022-01-26 17:51:51 -0500

    rajout test openDialog

--- client/src/app/pages/game-selection-page/game-selection-page.component.spec.ts
+++ client/src/app/pages/game-selection-page/game-selection-page.component.spec.ts
@@ -1,17 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDialogRef } from '@angular/material/dialog';
+import { FormBuilder } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppMaterialModule } from '@app/modules/material.module';
 import { GameSelectionPageComponent } from './game-selection-page.component';
 
 describe('GameSelectionPageComponent', () => {
     let component: GameSelectionPageComponent;
     let fixture: ComponentFixture<GameSelectionPageComponent>;
+    const mockDialogSpy: { open: jasmine.Spy } = {
+        open: jasmine.createSpy('open'),
+    };
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [GameSelectionPageComponent],
-            imports: [AppMaterialModule],
-            providers: [{ provide: MatDialogRef, useValue: {} }],
+            imports: [AppMaterialModule, BrowserAnimationsModule],
+            providers: 
+            [
+                {
+                 provide: MatDialog, 
+                 useValue: mockDialogSpy,
+                },
+                FormBuilder,
+            ],
         }).compileComponents();
     });
 
@@ -23,5 +35,10 @@ describe('GameSelectionPageComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should close the window when the begin button is clicked', () => {
+        component.openDialog();
+        expect(mockDialogSpy.open).toHaveBeenCalled();
     });
 });
