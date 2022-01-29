d1843c07dd49cc3203f38a5e4c49795782a173bf
theolouesdon <65035689+theolouesdon@users.noreply.github.com>
2022-01-28 22:08:09 -0500

    Fixe probleme build

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { AppComponent } from '@app/pages/app/app.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
+import { ConfirmSurrenderComponent } from './components/confirm-surrender/confirm-surrender.component';
+import { SurrenderGameComponent } from './components/surrender-game/surrender-game.component';
 
 /**
  * Main module that is used in main.ts.
@@ -19,7 +21,16 @@ import { MaterialPageComponent } from '@app/pages/material-page/material-page.co
  * Otherwise Angular Cli will not know in which module to put new component
  */
 @NgModule({
-    declarations: [AppComponent, GamePageComponent, MainPageComponent, MaterialPageComponent, PlayAreaComponent, SidebarComponent],
+    declarations: [
+        AppComponent,
+        GamePageComponent,
+        MainPageComponent,
+        MaterialPageComponent,
+        PlayAreaComponent,
+        SidebarComponent,
+        SurrenderGameComponent,
+        ConfirmSurrenderComponent,
+    ],
     imports: [AppMaterialModule, AppRoutingModule, BrowserAnimationsModule, BrowserModule, FormsModule, HttpClientModule],
     providers: [],
     bootstrap: [AppComponent],
