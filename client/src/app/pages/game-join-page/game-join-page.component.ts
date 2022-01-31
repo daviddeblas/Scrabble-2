1f3276515466dfb32c809fb343ae4d81930376c6
tursh <raphaeltremblay01@gmail.com>
2022-01-31 10:25:25 -0500

    Ajout du form pour le nom

--- client/src/app/pages/game-join-page/game-join-page.component.ts
+++ client/src/app/pages/game-join-page/game-join-page.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-game-join-page',
-  templateUrl: './game-join-page.component.html',
-  styleUrls: ['./game-join-page.component.scss']
+    selector: 'app-game-join-page',
+    templateUrl: './game-join-page.component.html',
+    styleUrls: ['./game-join-page.component.scss'],
 })
 export class GameJoinPageComponent implements OnInit {
+    formGroup: FormGroup;
 
-  constructor() { }
+    constructor(formBuilder: FormBuilder) {
+        this.formGroup = formBuilder.group({
+            name: ['', Validators.required],
+        });
+    }
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
