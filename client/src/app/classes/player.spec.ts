5a639200c6db0880b9394bbd86d6efd21c3d11ec
Raphael Tremblay <raphaeltremblay01@gmail.com>
2022-01-30 22:26:49 -0500

    Ajout des tests de player

---
+++ client/src/app/classes/player.spec.ts
@@ -0,0 +1,44 @@
+import { Letter } from './letter';
+import { Player } from './player';
+
+describe('Player', () => {
+    it('should create an instance', () => {
+        const player = new Player('Player 1');
+        expect(player).toBeTruthy();
+        expect(player.name).toEqual('Player 1');
+        expect(player.easel).toEqual([]);
+        expect(player.score).toEqual(0);
+    });
+
+    describe('Easel', () => {
+        const letters: Letter[] = ['I', 'U', 'O'];
+
+        let player: Player;
+
+        beforeEach(() => {
+            player = new Player('Player 1');
+        });
+
+        it('should add letter to easel', () => {
+            player.addLettersToEasel(letters);
+            expect(player.easel).toEqual(letters);
+
+            player.addLettersToEasel(letters);
+            expect(player.easel).toEqual([...letters, ...letters]);
+        });
+
+        it('should cause error if exceed easel capacity', () => {
+            player.addLettersToEasel(letters);
+            player.addLettersToEasel(letters);
+            expect(() => {
+                player.addLettersToEasel(letters);
+            }).toThrow(new Error('The easel capacity has been exceeded'));
+        });
+
+        it('should remove letter from easel', () => {
+            player.easel = ['I', 'A', 'O', 'U', 'Y', 'W'];
+            player.removeLettersFromEasel(letters);
+            expect(player.easel).toEqual(['A', 'Y', 'W']);
+        });
+    });
+});
