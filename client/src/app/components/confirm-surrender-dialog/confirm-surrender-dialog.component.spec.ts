b0217ffd6a9ada65c1d63f563c2c00dbe5711fa0
DavidDeBlas <71023594+daviddeblas@users.noreply.github.com>
2022-01-31 12:11:42 -0500

    Rajout test dialog

--- client/src/app/components/confirm-surrender-dialog/confirm-surrender-dialog.component.spec.ts
+++ client/src/app/components/confirm-surrender-dialog/confirm-surrender-dialog.component.spec.ts
@@ -1,13 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AppMaterialModule } from '@app/modules/material.module';
 import { ConfirmSurrenderDialogComponent } from './confirm-surrender-dialog.component';
 
 describe('ConfirmSurrenderComponent', () => {
     let component: ConfirmSurrenderDialogComponent;
     let fixture: ComponentFixture<ConfirmSurrenderDialogComponent>;
+    const mockDialogSpy: { close: jasmine.Spy } = {
+        close: jasmine.createSpy('close'),
+    };
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [ConfirmSurrenderDialogComponent],
+            imports: [AppMaterialModule],
+            providers: [ 
+                { 
+                    provide: MAT_DIALOG_DATA, 
+                    useValue: {} },
+                {
+                    provide: MatDialogRef,
+                    useValue: mockDialogSpy,
+                }
+            ]
+                
         }).compileComponents();
     });
 
@@ -19,5 +35,16 @@ describe('ConfirmSurrenderComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should close the window when the accept button is clicked', () => {
+        component.closeDialog();
+        expect(mockDialogSpy.close).toHaveBeenCalled();
+    });
+
+    it('should call the closeDialog method when surrenderGame is called', () => {
+        const spy = spyOn(component, "closeDialog");
+        component.surrenderGame();
+        expect(spy).toHaveBeenCalled();
     });
 });
