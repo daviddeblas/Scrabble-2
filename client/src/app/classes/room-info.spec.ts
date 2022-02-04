c1fb0039b8e68189b18fc76a767a9624b98a8eec
Raphael Tremblay <raphaeltremblay01@gmail.com>
2022-02-04 12:21:26 -0500

    Fix room-info test

--- client/src/app/classes/room-info.spec.ts
+++ client/src/app/classes/room-info.spec.ts
@@ -1,7 +1,13 @@
+import { GameOptions } from './game-options';
 import { RoomInfo } from './room-info';
 
 describe('RoomInfo', () => {
     it('should create an instance', () => {
-        expect(new RoomInfo('Host 1', 'Room id')).toBeTruthy();
+        const timer = 60;
+        const gameOptions = new GameOptions('Host 1', 'Mon Dictionaire', timer);
+        const room = new RoomInfo('Room id', gameOptions);
+        expect(room.gameOptions.hostname).toEqual('Host 1');
+        expect(room.roomId).toEqual('Room id');
+        expect(room.gameOptions).toEqual(gameOptions);
     });
 });
