54f50efca5bd0ec9cc2b3f9ac51c08ac8b5df8cf
theolouesdon <65035689+theolouesdon@users.noreply.github.com>
2022-02-05 19:19:15 -0500

    Enlever du code mort

--- client/src/app/pages/game-page/game-page.component.ts
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -1,18 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import * as chatActions from '@app/actions/chat.actions';
-import { Store } from '@ngrx/store';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-game-page',
     templateUrl: './game-page.component.html',
     styleUrls: ['./game-page.component.scss'],
 })
-export class GamePageComponent implements OnInit {
-    constructor(private store: Store) {}
-
-    ngOnInit() {
-        for (let i = 0; i < 100; ++i) {
-            this.store.dispatch(chatActions.sendMsg({ username: 'Raph', message: 'msg #' + i }));
-        }
-    }
+export class GamePageComponent {
+    constructor() {}
 }
