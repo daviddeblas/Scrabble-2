cc3b8cffad1088a76d6ddd58d31c54cbd9f9c2b4
theolouesdon <65035689+theolouesdon@users.noreply.github.com>
2022-02-05 19:17:24 -0500

    Création d'actions pour le chat

--- client/src/app/actions/chat.actions.ts
+++ client/src/app/actions/chat.actions.ts
@@ -1,3 +1,7 @@
 import { createAction, props } from '@ngrx/store';
 
-export const sendMsg = createAction('[Chat] Send msg', props<{ username: string; message: string }>());
+// export const sendMsg = createAction('[Chat] Send msg', props<{ username: string; message: string }>());
+
+export const messageWritten = createAction('[Chat] Message written', props<{ username: string; message: string }>());
+
+export const chatMessageReceived = createAction('[Chat] Received message', props<{ username: string; message: string }>());
