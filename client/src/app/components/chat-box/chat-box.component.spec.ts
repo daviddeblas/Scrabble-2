8e4d5647d02520fa84da6871473482537b856565
theolouesdon <65035689+theolouesdon@users.noreply.github.com>
2022-02-06 14:06:41 -0500

    Test de la fonction submit

--- client/src/app/components/chat-box/chat-box.component.spec.ts
+++ client/src/app/components/chat-box/chat-box.component.spec.ts
@@ -1,14 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { messageWritten } from '@app/actions/chat.actions';
+import { AppMaterialModule } from '@app/modules/material.module';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { cold } from 'jasmine-marbles';
 import { ChatBoxComponent } from './chat-box.component';
 
 describe('ChatBoxComponent', () => {
     let component: ChatBoxComponent;
     let fixture: ComponentFixture<ChatBoxComponent>;
-
+    let store: MockStore;
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [ChatBoxComponent],
+            imports: [AppMaterialModule, BrowserAnimationsModule],
+            providers: [provideMockStore()],
         }).compileComponents();
+        store = TestBed.inject(MockStore);
     });
 
     beforeEach(() => {
@@ -19,5 +27,24 @@ describe('ChatBoxComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should dispatch "[Chat] Message written" when submitted', () => {
+        const expectedUsername = 'My username';
+        const expectedMessage = 'My Message';
+        component.chatMessage.nativeElement.value = expectedMessage;
+        component.username = expectedUsername;
+        fixture.detectChanges();
+
+        component.submitMessage();
+        const expectedAction = cold('a', { a: messageWritten({ username: expectedUsername, message: expectedMessage }) });
+        expect(store.scannedActions$).toBeObservable(expectedAction);
+    });
+
+    it('should reset the chatMessage value when submitted', () => {
+        component.chatMessage.nativeElement.value = 'My message';
+        fixture.detectChanges();
+        component.submitMessage();
+        expect(component.chatMessage.nativeElement.value).toEqual('');
     });
 });
