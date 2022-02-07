ee0c048ef443d77643e0bd7dc5a68b2261837408
theolouesdon <65035689+theolouesdon@users.noreply.github.com>
2022-02-07 14:47:22 -0500

    Retirage de code mort

--- client/src/app/actions/chat.actions.ts
+++ client/src/app/actions/chat.actions.ts
@@ -1,13 +1,11 @@
 import { createAction, props } from '@ngrx/store';
 
-// export const sendMsg = createAction('[Chat] Send msg', props<{ username: string; message: string }>());
-
 export const initiateChatting = createAction('[Chat] Initiate chatting');
 
 export const messageWritten = createAction('[Chat] Message written', props<{ username: string; message: string }>());
 
 export const chatMessageReceived = createAction('[Chat] Received message', props<{ username: string; message: string }>());
 
-export function acceptNewMessages() {
+export const acceptNewMessages = () => {
     throw new Error('Function not implemented.');
-}
+};
