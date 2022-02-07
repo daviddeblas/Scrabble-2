99061ffc8320a390f8da16d466e3cfa3df809139
theolouesdon <65035689+theolouesdon@users.noreply.github.com>
2022-02-07 11:02:33 -0500

    Ajout getter de username du joueur

--- client/src/app/components/chat-box/chat-box.component.ts
+++ client/src/app/components/chat-box/chat-box.component.ts
@@ -1,8 +1,10 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as chatActions from '@app/actions/chat.actions';
 import { ChatMessage } from '@app/classes/chat-message';
+import { Players } from '@app/reducers/player.reducer';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
     selector: 'app-chat-box',
@@ -12,16 +14,18 @@ import { Observable } from 'rxjs';
 export class ChatBoxComponent implements OnInit {
     @ViewChild('chatMessage') chatMessage: ElementRef<HTMLInputElement>;
     chat$: Observable<ChatMessage[]>;
+    players$: Observable<string>;
     username: string;
-    constructor(private store: Store<{ chat: ChatMessage[] }>) {
+    constructor(private store: Store<{ chat: ChatMessage[] }>, private playerStore: Store<{ players: Players }>) {
         this.chat$ = store.select('chat');
-        this.username = 'Raph';
+        this.players$ = playerStore.select('players', 'player', 'name');
     }
     ngOnInit(): void {
         this.store.dispatch(chatActions.initiateChatting());
     }
 
     submitMessage(): void {
+        this.playerStore.pipe(take(1)).subscribe((us) => (this.username = us.players.player.name));
         this.store.dispatch(chatActions.messageWritten({ username: this.username, message: this.chatMessage.nativeElement.value }));
         this.chatMessage.nativeElement.value = '';
     }
