c44c7c651ae35d63aa654c9451798a46e13a824a
musique88 <musique88181@gmail.com>
2022-02-08 17:14:02 -0500

    board score

--- server/app/classes/game/board.ts
+++ server/app/classes/game/board.ts
@@ -1,25 +1,26 @@
-import { Letter } from '@app/classes/letter';
-import { Multiplier } from '@app/classes/multiplier';
-import { PlacedLetter } from '@app/classes/placed-letter';
 import { GameConfig } from '@app/classes/game-config';
+import { GameError, GameErrorType } from '@app/classes/game.exception';
+import { Letter } from '@app/classes/letter';
+import { Multiplier, MultiplierType } from '@app/classes/multiplier';
+import { PlacedLetter } from '@app/classes/placed-letter';
+import { Vec2 } from '@app/classes/vec2';
 
 export class Board {
     board: (Letter | null)[][];
     multipliers: (Multiplier | null)[][];
     pointsPerLetter: Map<Letter, number>;
+    blanks: Vec2[];
+    size: Vec2;
 
     constructor(config: GameConfig) {
-        this.board = new Array(config.boardSize.x);
+        this.size = new Vec2(config.boardSize.x, config.boardSize.y);
+        this.board = new Array(this.size.x);
+        this.multipliers = new Array(config.boardSize.x);
         for (let i = 0; i < this.board.length; i++) {
-            this.board[i] = new Array(config.boardSize.y);
+            this.multipliers[i] = new Array(config.boardSize.y);
+            this.board[i] = new Array(this.size.y);
             for (let j = 0; j < this.board.length; j++) {
                 this.board[i][j] = null;
-            }
-        }
-        this.multipliers = new Array(config.boardSize.x);
-        for (let i = 0; i < this.multipliers.length; i++) {
-            this.multipliers[i] = new Array(config.boardSize.y);
-            for (let j = 0; j < this.multipliers.length; j++) {
                 this.multipliers[i][j] = null;
             }
         }
@@ -31,11 +32,53 @@ export class Board {
         );
     }
 
-    place(letters: PlacedLetter[]): number {
+    place(letters: PlacedLetter[]): number {}
 
+    scorePositions(pos: Vec2[]): number {
+        let score = 0;
+        let multiplier = 1;
+        pos.forEach((vec) => {
+            // get current letter
+            const letter = this.board[vec.x][vec.y];
+            if (letter === null) throw new GameError(GameErrorType.LetterIsNull);
+            // prendre ne nombre de points associe a cette lettre
+            const letterPoints = this.pointsPerLetter.get(letter);
+            if (letterPoints === undefined) throw new GameError(GameErrorType.UndefinedPoints);
+            // annuler s'il s'agit d'un blank
+            if (this.blanks.findIndex((p) => p.equals(vec)) < 0) return;
+            // obtenir le multiplieur a cette position
+            const multi = this.multipliers[vec.x][vec.y];
+            if (multi === null) {
+                score += letterPoints;
+                return;
+            }
+            switch (multi.type) {
+                case MultiplierType.Letter:
+                    score += letterPoints * (multi.type === MultiplierType.Letter ? multi.amt : 1);
+                    break;
+                case MultiplierType.Word:
+                    score += letterPoints;
+                    multiplier = multiplier < multi.amt ? multi.amt : multiplier;
+                    break;
+            }
+        });
+        score *= multiplier;
+        return score;
     }
 
     wordsGenerated(letters: PlacedLetter[]): string[] {
-
+        const tempBoard: (Letter | null)[][] = new Array(this.size.x);
+        // copie board dans tempBoard en profondeur
+        for (let i = 0; i < tempBoard.length; i++) {
+            tempBoard[i] = new Array(this.size.y);
+            for (let j = 0; j < tempBoard.length; j++) {
+                tempBoard[i][j] = this.board[i][j];
+            }
+        }
+        letters.forEach((l) => {
+            tempBoard[l.position.x][l.position.y] = l.letter;
+        });
     }
+
+    checkWord(direction: Vec2, pos: Vec2): string {}
 }
