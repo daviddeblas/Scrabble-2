b96d5791ae56ade3fb3618e2e48dfb484bd994ed
nico <23260358+nico14916@users.noreply.github.com>
2022-02-09 11:24:13 -0500

    easel: ajout lettres + style

--- client/src/app/components/easel/easel.component.ts
+++ client/src/app/components/easel/easel.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BoardState } from '@app/reducers/board.reducer';
+import { Players } from '@app/reducers/player.reducer';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
@@ -10,8 +11,10 @@ import { Observable } from 'rxjs';
 })
 export class EaselComponent {
     boardState$: Observable<BoardState>;
+    players$: Observable<Players>;
 
-    constructor(store: Store<{ boardState: BoardState }>) {
+    constructor(store: Store<{ boardState: BoardState; players: Players }>) {
         this.boardState$ = store.select('boardState');
+        this.players$ = store.select('players');
     }
 }
