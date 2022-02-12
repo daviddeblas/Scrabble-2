4bcc4eaa52ea022d05657929e7b91ab65737d74c
Raphael Tremblay <raphaeltremblay01@gmail.com>
2022-02-12 16:04:01 -0500

    Ajout de la validation du nom dans join-page

--- client/src/app/pages/game-join-page/game-join-page.component.ts
+++ client/src/app/pages/game-join-page/game-join-page.component.ts
@@ -1,13 +1,21 @@
 import { Component, ViewChild } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MatStepper } from '@angular/material/stepper';
 import { cancelJoinRoom, joinRoom, loadRooms } from '@app/actions/room.actions';
 import { RoomInfo } from '@app/classes/room-info';
+import { MAX_NAME_LENGTH, MIN_NAME_LENGTH } from '@app/constants';
 import { RoomEffects } from '@app/effects/room.effects';
 import { RoomState } from '@app/reducers/room.reducer';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+
+export const forbiddenNameValidator = (name: string) => {
+    return (control: AbstractControl): ValidationErrors | null => {
+        const forbidden = name === control.value;
+        return forbidden ? { forbiddenName: { value: control.value } } : null;
+    };
+};
 
 @Component({
     selector: 'app-game-join-page',
@@ -17,12 +25,15 @@ import { Observable } from 'rxjs';
 export class GameJoinPageComponent {
     @ViewChild(MatStepper) stepper: MatStepper;
 
+    readonly minNameLength: number = MIN_NAME_LENGTH;
+    readonly maxNameLength: number = MAX_NAME_LENGTH;
+
     formGroup: FormGroup;
     selectedRoom: RoomInfo | undefined;
     isFormDisabled: boolean = false;
 
     roomList$: Observable<RoomInfo[]>;
-    pendingRooms$: Observable<RoomInfo | undefined>;
+    pendingRoom$: Observable<RoomInfo | undefined>;
 
     constructor(
         formBuilder: FormBuilder,
@@ -31,18 +42,21 @@ export class GameJoinPageComponent {
         private effects: RoomEffects,
         dialogRef: MatDialogRef<GameJoinPageComponent>,
     ) {
-        this.formGroup = formBuilder.group({
-            name: new FormControl({ value: '', disabled: this.isFormDisabled }),
-        });
+        this.formGroup = formBuilder.group({ name: new FormControl({ value: '', disabled: this.isFormDisabled }) });
 
         this.roomList$ = roomStore.select('room', 'roomList');
-        this.pendingRooms$ = roomStore.select('room', 'pendingRoom');
+        this.pendingRoom$ = roomStore.select('room', 'pendingRoom');
         this.store.dispatch(loadRooms());
         this.effects.dialogRef = dialogRef;
     }
 
     selectRoom(roomInfo: RoomInfo): void {
         this.selectedRoom = roomInfo;
+        this.setupNameValidators(roomInfo.gameOptions.hostname);
+    }
+
+    unSelectRoom(): void {
+        this.setupNameValidators('');
     }
 
     joinGame(): void {
@@ -55,5 +69,16 @@ export class GameJoinPageComponent {
         this.selectedRoom = undefined;
         this.formGroup.controls.name.enable();
         this.stepper.reset();
+    }
+
+    setupNameValidators(hostName: string): void {
+        this.formGroup.controls.name.clearValidators();
+        this.formGroup.controls.name.setValidators([
+            Validators.required,
+            Validators.minLength(this.minNameLength),
+            Validators.maxLength(this.maxNameLength),
+            forbiddenNameValidator(hostName),
+        ]);
+        this.formGroup.controls.name.updateValueAndValidity();
     }
 }
