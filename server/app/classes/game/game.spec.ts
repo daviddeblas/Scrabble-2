c57edd2137c3a575bb8e99c57836331b3c6700ba
musique88 <musique88181@gmail.com>
2022-02-13 01:32:06 -0500

    ajustement du test de constructeur

--- server/app/classes/game/game.spec.ts
+++ server/app/classes/game/game.spec.ts
@@ -18,9 +18,11 @@ describe('game', () => {
     it('constructor', () => {
         expect(game.players.length).to.eq(2);
         expect(game.activePlayer === 0 || game.activePlayer === 1).to.eq(true);
-        expect(game.bag.letters.length).to.eq(
-            game.config.letters.map((l) => l.amount).reduce((sum, amount) => sum + amount) - MAX_LETTERS_IN_EASEL * 2,
-        );
+        const amountOfEachLetters = game.config.letters.map((l) => l.amount);
+        const totalLetters = amountOfEachLetters.reduce((sum, amount) => sum + amount);
+        const totalLettersInEachPlayerEasel = MAX_LETTERS_IN_EASEL;
+        const totalAmountOfPlayers = 2;
+        expect(game.bag.letters.length).to.eq(totalLetters - totalLettersInEachPlayerEasel * totalAmountOfPlayers);
         expect(game.turnsSkipped).to.eq(0);
     });
 
