fde4b2df168d64a0ec587896a8f17d84feb8bcd3
nico <23260358+nico14916@users.noreply.github.com>
2022-02-13 17:00:32 -0500

    sidebar: ajout des noms de joueurs

--- client/src/app/components/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { Players } from '@app/reducers/player.reducer';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-sidebar',
     templateUrl: './sidebar.component.html',
     styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+    players$: Observable<Players>;
+
+    constructor(store: Store<{ players: Players }>) {
+        this.players$ = store.select('players');
+    }
+}
