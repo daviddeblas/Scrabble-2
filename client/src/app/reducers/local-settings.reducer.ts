df4b947bda6163cdfc6612a0b36f59e7dd1260ae
nico <23260358+nico14916@users.noreply.github.com>
2022-02-14 12:12:43 -0500

    localSettings: limiter le zoom

--- client/src/app/reducers/local-settings.reducer.ts
+++ client/src/app/reducers/local-settings.reducer.ts
@@ -1,6 +1,10 @@
 import { zoomIn, zoomOut } from '@app/actions/local-settings.actions';
 import { createReducer, on } from '@ngrx/store';
 
+const ZOOM_MAX = 1.5;
+const ZOOM_MIN = 1.0;
+const ZOOM_STEP = 0.1;
+
 export const localSettingsFeatureKey = 'localSettings';
 
 export interface LocalSettings {
@@ -12,11 +16,15 @@ export const initialState: LocalSettings = { zoom: 1 };
 export const reducer = createReducer(
     initialState,
     on(zoomIn, (state) => {
-        state.zoom += 0.1;
-        return state;
+        if (state.zoom + ZOOM_STEP > ZOOM_MAX) {
+            return { ...state, zoom: ZOOM_MAX };
+        }
+        return { ...state, zoom: state.zoom + ZOOM_STEP };
     }),
     on(zoomOut, (state) => {
-        state.zoom -= 0.1;
-        return state;
+        if (state.zoom - ZOOM_STEP < ZOOM_MIN) {
+            return { ...state, zoom: ZOOM_MIN };
+        }
+        return { ...state, zoom: state.zoom - ZOOM_STEP };
     }),
 );
