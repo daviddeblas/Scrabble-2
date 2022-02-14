0f370f52d6e636433c71fdf2c37d0aab53064a8e
nico <23260358+nico14916@users.noreply.github.com>
2022-02-14 12:50:11 -0500

    sidebar component: correction test

--- client/src/app/components/sidebar/sidebar.component.spec.ts
+++ client/src/app/components/sidebar/sidebar.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { StoreModule } from '@ngrx/store';
 import { SidebarComponent } from './sidebar.component';
 
 describe('SidebarComponent', () => {
@@ -8,6 +9,7 @@ describe('SidebarComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [SidebarComponent],
+            imports: [StoreModule.forRoot({})],
         }).compileComponents();
     });
 
