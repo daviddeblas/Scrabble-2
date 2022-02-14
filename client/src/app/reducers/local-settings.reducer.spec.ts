014e67730fe0d56f74a5d55fe0824f066f4b69c6
nico <23260358+nico14916@users.noreply.github.com>
2022-02-14 12:48:06 -0500

    localSettings reducer: test zoomOut

--- client/src/app/reducers/local-settings.reducer.spec.ts
+++ client/src/app/reducers/local-settings.reducer.spec.ts
@@ -1,5 +1,5 @@
-import { zoomIn } from '@app/actions/local-settings.actions';
-import { reducer, ZOOM_MAX, ZOOM_STEP } from './local-settings.reducer';
+import { zoomIn, zoomOut } from '@app/actions/local-settings.actions';
+import { reducer, ZOOM_MAX, ZOOM_MIN, ZOOM_STEP } from './local-settings.reducer';
 
 describe('LocalSettings Reducer', () => {
     describe('zoomIn action', () => {
@@ -18,6 +18,26 @@ describe('LocalSettings Reducer', () => {
             const result = reducer({ zoom: currentZoom }, action);
 
             const expected = { zoom: ZOOM_MAX };
+            expect(result).toEqual(expected);
+        });
+    });
+
+    describe('zoomOut action', () => {
+        it('should zoom out', () => {
+            const currentZoom = 1.3;
+            const action = zoomOut();
+            const result = reducer({ zoom: currentZoom }, action);
+
+            const expected = { zoom: currentZoom - ZOOM_STEP };
+            expect(result).toEqual(expected);
+        });
+
+        it('should not be under minimum', () => {
+            const currentZoom = ZOOM_MIN + ZOOM_STEP / 2;
+            const action = zoomOut();
+            const result = reducer({ zoom: currentZoom }, action);
+
+            const expected = { zoom: ZOOM_MIN };
             expect(result).toEqual(expected);
         });
     });
