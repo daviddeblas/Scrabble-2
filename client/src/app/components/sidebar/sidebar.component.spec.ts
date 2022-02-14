2c1cae975316700823e51e8914d9a3661a6e3aa6
nico <23260358+nico14916@users.noreply.github.com>
2022-02-14 17:56:40 -0500

    sidebar component: ajout tests

--- client/src/app/components/sidebar/sidebar.component.spec.ts
+++ client/src/app/components/sidebar/sidebar.component.spec.ts
@@ -1,15 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { StoreModule } from '@ngrx/store';
+import { zoomIn, zoomOut } from '@app/actions/local-settings.actions';
+import { Store } from '@ngrx/store';
 import { SidebarComponent } from './sidebar.component';
 
 describe('SidebarComponent', () => {
     let component: SidebarComponent;
     let fixture: ComponentFixture<SidebarComponent>;
+    let store: jasmine.SpyObj<Store>;
 
     beforeEach(async () => {
+        store = jasmine.createSpyObj<Store>('store', ['dispatch', 'select']);
+
         await TestBed.configureTestingModule({
             declarations: [SidebarComponent],
-            imports: [StoreModule.forRoot({})],
+            providers: [
+                {
+                    provide: Store,
+                    useValue: store,
+                },
+            ],
         }).compileComponents();
     });
 
@@ -21,5 +30,15 @@ describe('SidebarComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should dispatch "[LocalSettings] Zoom In"', () => {
+        component.zoomIn();
+        expect(store.dispatch).toHaveBeenCalledWith(zoomIn());
+    });
+
+    it('should dispatch "[LocalSettings] Zoom Out"', () => {
+        component.zoomOut();
+        expect(store.dispatch).toHaveBeenCalledWith(zoomOut());
     });
 });
