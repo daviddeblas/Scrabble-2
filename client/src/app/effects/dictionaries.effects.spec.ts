0ec775815a543a3482939b14ff9351c86baa4a57
tursh <raphaeltremblay01@gmail.com>
2022-02-14 12:15:53 -0500

    Ajout des tests de dictionary effects

--- client/src/app/effects/dictionaries.effects.spec.ts
+++ client/src/app/effects/dictionaries.effects.spec.ts
@@ -1,16 +1,27 @@
 import { TestBed } from '@angular/core/testing';
+import { loadDictionaries } from '@app/actions/dictionaries.actions';
+import { DictionaryService } from '@app/services/dictionary.service';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { provideMockStore } from '@ngrx/store/testing';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { DictionariesEffects } from './dictionaries.effects';
 
 describe('DictionariesEffects', () => {
     let actions$: Observable<unknown>;
     let effects: DictionariesEffects;
 
+    let dictionaryService: jasmine.SpyObj<DictionaryService>;
+
     beforeEach(() => {
+        dictionaryService = jasmine.createSpyObj('dictionaryService', ['getDictionaries']);
+
         TestBed.configureTestingModule({
-            providers: [DictionariesEffects, provideMockActions(() => actions$), provideMockStore()],
+            providers: [
+                DictionariesEffects,
+                provideMockActions(() => actions$),
+                provideMockStore(),
+                { provide: DictionaryService, useValue: dictionaryService },
+            ],
         });
 
         effects = TestBed.inject(DictionariesEffects);
@@ -18,5 +29,11 @@ describe('DictionariesEffects', () => {
 
     it('should be created', () => {
         expect(effects).toBeTruthy();
+    });
+
+    it('loadDictionaries$ should call getDictionaries from dictionary service', () => {
+        actions$ = of(loadDictionaries);
+        effects.loadDictionaries$.subscribe();
+        expect(dictionaryService.getDictionaries).toHaveBeenCalled();
     });
 });
