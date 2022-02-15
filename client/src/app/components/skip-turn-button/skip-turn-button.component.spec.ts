0a616bae2054e7c569e32af5fdad320ee691a268
nico <23260358+nico14916@users.noreply.github.com>
2022-02-15 14:32:50 -0500

    skip turn button: tests

--- client/src/app/components/skip-turn-button/skip-turn-button.component.spec.ts
+++ client/src/app/components/skip-turn-button/skip-turn-button.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { messageWritten } from '@app/actions/chat.actions';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { cold } from 'jasmine-marbles';
 import { SkipTurnButtonComponent } from './skip-turn-button.component';
 
 describe('SkipTurnButtonComponent', () => {
     let component: SkipTurnButtonComponent;
     let fixture: ComponentFixture<SkipTurnButtonComponent>;
+    let store: MockStore;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [SkipTurnButtonComponent],
+            providers: [provideMockStore()],
         }).compileComponents();
+
+        store = TestBed.inject(MockStore);
     });
 
     beforeEach(() => {
@@ -19,5 +26,24 @@ describe('SkipTurnButtonComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should dispatch "[Chat] Message written"', () => {
+        component.username = 'HelloWorld';
+        const expectedAction = cold('a', { a: messageWritten({ username: 'HelloWorld', message: '!passer' }) });
+        component.skipTurn();
+        expect(store.scannedActions$).toBeObservable(expectedAction);
+    });
+
+    it('should return true when it is player turn', () => {
+        component.username = 'HelloWorld';
+        component.activePlayer = 'HelloWorld';
+        expect(component.isPlayerTurn()).toBeTrue();
+    });
+
+    it('should return false when it is not player turn', () => {
+        component.username = 'HelloWorld';
+        component.activePlayer = 'ByeWorld';
+        expect(component.isPlayerTurn()).toBeFalse();
     });
 });
