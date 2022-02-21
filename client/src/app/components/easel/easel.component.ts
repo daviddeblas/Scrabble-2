c4790f8310b1a2426f3d009ea74eb2fa34a5f76a
theolouesdon <65035689+theolouesdon@users.noreply.github.com>
2022-02-21 17:10:04 -0500

    Ajout d'une interaction avec les lettres du chevalet

--- client/src/app/components/easel/easel.component.ts
+++ client/src/app/components/easel/easel.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Letter } from '@app/classes/letter';
+import { MAX_EASEL_SIZE } from '@app/constants';
 import { BoardState } from '@app/reducers/board.reducer';
 import { Players } from '@app/reducers/player.reducer';
 import { Store } from '@ngrx/store';
@@ -11,11 +12,31 @@ import { Observable } from 'rxjs';
     styleUrls: ['./easel.component.scss'],
 })
 export class EaselComponent {
+    readonly mainColor = '#fffcec';
+    readonly exchangeColor = 'red';
+    readonly maxEaselSize = MAX_EASEL_SIZE;
     pointsPerLetter$: Observable<Map<Letter, number>>;
     players$: Observable<Players>;
+    letterColor: string[];
+    playerIsActive: boolean = false;
 
     constructor(store: Store<{ board: BoardState; players: Players }>) {
         this.pointsPerLetter$ = store.select('board', 'pointsPerLetter');
         this.players$ = store.select('players');
+        this.letterColor = new Array(this.maxEaselSize).fill(this.mainColor);
+    }
+
+    selectLetterToSwitch(event: MouseEvent, letterIndex: number): void {
+        event.preventDefault();
+        const color = this.letterColor[letterIndex];
+        if (color === this.exchangeColor) {
+            this.letterColor[letterIndex] = this.mainColor;
+        } else if (color === this.mainColor) {
+            this.letterColor[letterIndex] = this.exchangeColor;
+        }
+    }
+
+    letterSelected(): boolean {
+        return this.letterColor.includes(this.exchangeColor);
     }
 }
