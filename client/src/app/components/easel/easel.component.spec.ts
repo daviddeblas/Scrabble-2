045d4af0e9e74868c29ec69c8a70ac57dcac3166
DavidDeBlas <daviddeblas@ymail.com>
2022-02-23 15:25:47 -0500

    rajout test easel

--- client/src/app/components/easel/easel.component.spec.ts
+++ client/src/app/components/easel/easel.component.spec.ts
@@ -6,6 +6,7 @@ import { EaselComponent } from './easel.component';
 describe('EaselComponent', () => {
     let component: EaselComponent;
     let fixture: ComponentFixture<EaselComponent>;
+    let mouseClickStub: MouseEvent;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
@@ -13,6 +14,11 @@ describe('EaselComponent', () => {
             imports: [AppMaterialModule],
             providers: [provideMockStore()],
         }).compileComponents();
+        mouseClickStub = {
+            preventDefault: () => {
+                return;
+            },
+        } as unknown as MouseEvent;
     });
 
     beforeEach(() => {
@@ -23,5 +29,33 @@ describe('EaselComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should call event.preventDefault when selectLetterToSwitch is called', () => {
+        const spy = spyOn(mouseClickStub, 'preventDefault');
+        component.selectLetterToSwitch(mouseClickStub, 0);
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('should test if the letter selected has the main color', () => {
+        component.letterColor[0] = component.exchangeColor;
+        component.selectLetterToSwitch(mouseClickStub, 0);
+        expect(component.letterColor[0]).toEqual(component.mainColor);
+    });
+
+    it('should test if the letter selected has the exchange color', () => {
+        component.letterColor[0] = '';
+        component.selectLetterToSwitch(mouseClickStub, 0);
+        expect(component.letterColor[0]).toEqual(component.exchangeColor);
+    });
+
+    it('should test if the letter color has exchange color', () => {
+        component.letterColor[0] = component.exchangeColor;
+        expect(component.letterSelected()).toBeTruthy();
+    });
+
+    it('should test if the letter color does not include exchange color', () => {
+        component.letterColor[0] = '';
+        expect(component.letterSelected()).toBeFalsy();
     });
 });
