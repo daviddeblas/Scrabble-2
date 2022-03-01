6d868e3389104478b57903adefc7af86d4ae1c2d
theolouesdon <65035689+theolouesdon@users.noreply.github.com>
2022-03-01 15:36:30 -0500

    Fix des tests après les modifs faites au MultiConfigWindow

--- client/src/app/pages/game-preparation-page/game-preparation-page.component.spec.ts
+++ client/src/app/pages/game-preparation-page/game-preparation-page.component.spec.ts
@@ -2,16 +2,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { createRoom } from '@app/actions/room.actions';
+import { GameOptions } from '@app/classes/game-options';
 import { MultiConfigWindowComponent } from '@app/components/multi-config-window/multi-config-window.component';
 import { WaitingRoomComponent } from '@app/components/waiting-room/waiting-room.component';
 import { AppMaterialModule } from '@app/modules/material.module';
-import { provideMockStore } from '@ngrx/store/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { cold } from 'jasmine-marbles';
 import { GamePreparationPageComponent } from './game-preparation-page.component';
 
 describe('GamePreparationPageComponent', () => {
     let component: GamePreparationPageComponent;
     let fixture: ComponentFixture<GamePreparationPageComponent>;
-    let multiConfigWindowMock: MultiConfigWindowComponent;
+    let store: MockStore;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
@@ -23,13 +26,10 @@ describe('GamePreparationPageComponent', () => {
                     provide: MatDialogRef,
                     useValue: {},
                 },
-                {
-                    provide: MultiConfigWindowComponent,
-                    useValue: multiConfigWindowMock,
-                },
                 provideMockStore(),
             ],
         }).compileComponents();
+        store = TestBed.inject(MockStore);
     });
 
     beforeEach(() => {
@@ -51,5 +51,12 @@ describe('GamePreparationPageComponent', () => {
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         component.multiConfigWindowComponent = undefined!;
         expect(component.formSettings).toEqual(component.firstFormGroup);
+    });
+
+    it('should dispatch createRoom with the gameOptions when onGameOptionsSubmit called', () => {
+        const expectedOptions = { name: 'My Name' } as unknown as GameOptions;
+        component.onGameOptionsSubmit(expectedOptions);
+        const expectedAction = cold('a', { a: createRoom({ gameOptions: expectedOptions }) });
+        expect(store.scannedActions$).toBeObservable(expectedAction);
     });
 });
--- client/src/app/pages/solo-game-settings-page/solo-game-settings-page.component.spec.ts
+++ client/src/app/pages/solo-game-settings-page/solo-game-settings-page.component.spec.ts
@@ -1,4 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MultiConfigWindowComponent } from '@app/components/multi-config-window/multi-config-window.component';
+import { AppMaterialModule } from '@app/modules/material.module';
+import { provideMockStore } from '@ngrx/store/testing';
 import { SoloGameSettingsPageComponent } from './solo-game-settings-page.component';
 
 describe('SoloGameSettingsPageComponent', () => {
@@ -7,7 +13,16 @@ describe('SoloGameSettingsPageComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [SoloGameSettingsPageComponent],
+            declarations: [SoloGameSettingsPageComponent, MultiConfigWindowComponent],
+            imports: [AppMaterialModule, BrowserAnimationsModule, ReactiveFormsModule],
+            providers: [
+                {
+                    provide: MatDialogRef,
+                    useValue: {},
+                },
+                FormBuilder,
+                provideMockStore(),
+            ],
         }).compileComponents();
     });
 
@@ -20,4 +35,13 @@ describe('SoloGameSettingsPageComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    // Test a ajusté une fois l'action créer
+    /* it('should dispatch createRoom with the gameOptions when onGameOptionsSubmit called', () => {
+        const expectedOptions = { name: 'My Name' } as unknown as GameOptions;
+        const expectedLevel = 'Débutant';
+        component.onGameOptionsSubmit(expectedOptions, expectedLevel);
+        const expectedAction = cold('a', { a: createRoom({ gameOptions: expectedOptions, botLevel: expectedLevel }) });
+        expect(store.scannedActions$).toBeObservable(expectedAction);
+    });*/
 });
