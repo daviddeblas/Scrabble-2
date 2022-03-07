e6bb765f38575da0d38c2043c29616bb3210c623
musique88 <musique88181@gmail.com>
2022-03-07 12:48:51 -0500

    modification de environnement

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    serverUrl: 'ec2-15-222-7-251.ca-central-1.compute.amazonaws.com',
+    serverUrl: 'http://ec2-15-222-7-251.ca-central-1.compute.amazonaws.com:3000',
 };
