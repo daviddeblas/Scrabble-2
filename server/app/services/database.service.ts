fce0b45845d4600c8e38ffaaf8254436351cb0aa
mymanat <71021952+mymanat@users.noreply.github.com>
2022-03-09 00:50:57 -0500

    ajout de la classe DatabaseService et ses attribut

--- server/app/services/database.service.ts
+++ server/app/services/database.service.ts
@@ -1,3 +1,23 @@
+import { Db, MongoClient, MongoClientOptions } from 'mongodb';
 const DATABASE_URL = 'mongodb+srv://log2990-101:<log2990-101>@cluster0.qzik0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const DATABASE_NAME = 'highScores';
 const DATABASE_COLLECTIONS = 'highScores_coll';
+
+export class DatabaseService {
+    private db: Db;
+    private client: MongoClient;
+    private options: MongoClientOptions = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    };
+
+    async start(url: string = DATABASE_URL): Promise<MongoClient | null> {
+        try {
+            const client = await MongoClient.connect(url, this.options);
+            this.client = client;
+            this.db = client.db(DATABASE_NAME);
+        } catch {
+            throw Error('Database connection error');
+        }
+}
+
