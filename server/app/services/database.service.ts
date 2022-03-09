0ca30298bc9fb93caaf375385c62753f435df9ec
mymanat <71021952+mymanat@users.noreply.github.com>
2022-03-09 00:44:50 -0500

    ajout des constantes MongoDB pour la manipulation de notre database

---
+++ server/app/services/database.service.ts
@@ -0,0 +1,3 @@
+const DATABASE_URL = 'mongodb+srv://log2990-101:<log2990-101>@cluster0.qzik0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const DATABASE_NAME = 'highScores';
+const DATABASE_COLLECTIONS = 'highScores_coll';
