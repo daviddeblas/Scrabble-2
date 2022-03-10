d991e43811842b0e5354abda344324156ca79d2b
mymanat <71021952+mymanat@users.noreply.github.com>
2022-03-09 22:57:45 -0500

    ajout fonction pour update le highscore

--- server/app/services/leaderboard.service.ts
+++ server/app/services/leaderboard.service.ts
@@ -3,11 +3,11 @@ import { DATABASE, DEFAULT_HIGHSCORE, HighScore } from '@app/classes/highscore';
 import { Db } from 'mongodb';
 
 export class LeaderboardService {
-    private highScoreDB: Db;
+    private highScore_DB: Db;
 
     async getHighscores(): Promise<HighScore[]> {
-        return this.highScoreDB
-            .collection(DATABASE.highScore.collections[GameMode === GameMode.Classical ? 'classical' : 'log2990'])
+        return this.highScore_DB
+            .collection(DATABASE.highScore.collections[gameMode === GameMode.Classical ? 'classical' : 'log2990'])
             .find({})
             .sort({ score: -1 })
             .toArray()
@@ -21,5 +21,14 @@ export class LeaderboardService {
             this.highScoreDB.collection(DATABASE.highScore.collections.classical).insertMany(DEFAULT_HIGHSCORE.classical),
             this.highScoreDB.collection(DATABASE.highScore.collections.log2290).insertMany(DEFAULT_HIGHSCORE.log2990),
         ]);
+    }
+
+    async updateHighScore(highScore: HighScore, gameMode: number): Promise<void> {
+        const collection = DATABASE.highScore.collections[gameMode === GameMode.Classical ? 'classical' : 'log2990'];
+        const lowestScore = await this.highScore_DB.collection(collection).find({}).sort({ score: 1 }).limit(1).toArray();
+        if (highScore.score > lowestScore[0].score) {
+            this.highScore_DB.collection(collection).insertOne(highScore);
+            this.highScore_DB.collection(collection).deleteOne({ id: (lowestScore[0] as Document).id });
+        }
     }
 }
