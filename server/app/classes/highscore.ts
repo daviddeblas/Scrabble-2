253f274221d83a9696b80e8a41d5bd40484301a3
mymanat <71021952+mymanat@users.noreply.github.com>
2022-03-09 20:32:43 -0500

    ajout de fonction reset database

--- server/app/classes/highscore.ts
+++ server/app/classes/highscore.ts
@@ -3,30 +3,30 @@ export interface HighScore {
     score: number;
 }
 
-export const DEFAULT_HIGHSCORE={
-    classical:[
-        {name: 'name1', score:0}
-        {name: 'name2', score:0}
-        {name: 'name3', score:0}
-        {name: 'name4', score:0}
-        {name: 'name5', score:0}
+export const DEFAULT_HIGHSCORE = {
+    classical: [
+        { name: 'name1', score: 0 },
+        { name: 'name2', score: 0 },
+        { name: 'name3', score: 0 },
+        { name: 'name4', score: 0 },
+        { name: 'name5', score: 0 },
     ] as HighScore[],
-    log2990:[
-        {name: 'name1', score:0}
-        {name: 'name2', score:0}
-        {name: 'name3', score:0}
-        {name: 'name4', score:0}
-        {name: 'name5', score:0}
+    log2990: [
+        { name: 'name1', score: 0 },
+        { name: 'name2', score: 0 },
+        { name: 'name3', score: 0 },
+        { name: 'name4', score: 0 },
+        { name: 'name5', score: 0 },
     ] as HighScore[],
 };
 
-export const DATABASE= {
-    url:'mongodb+srv://log2990-101:log2990-101@cluster0.qzik0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    highScore:{
-        name: 'HighScore',
+export const DATABASE = {
+    url: 'mongodb+srv://log2990-101:log2990-101@cluster0.qzik0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    highScore: {
+        name: 'Scores_DB',
         collections: {
-            classical: 'ScoreClassic',
-            log2290:'ScoreLog2990',
-        }
-    }
-}
+            classical: 'ScoresClassic',
+            log2290: 'ScoresLog2990',
+        },
+    },
+};
--- server/app/services/database.service.ts
+++ server/app/services/database.service.ts
@@ -1,28 +1,25 @@
+import { DATABASE } from '@app/classes/highscore';
 import { Db, MongoClient } from 'mongodb';
 
-const DATABASE_URL = 'mongodb+srv://log2990-101:<log2990-101>@cluster0.qzik0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const DATABASE_NAME = 'Scores_DB';
-const DATABASE_COLLECTIONS = 'Scores';
+// const DATABASE_URL = 'mongodb+srv://log2990-101:<log2990-101>@cluster0.qzik0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+// const DATABASE_NAME = 'Scores_DB';
+// const DATABASE_COLLECTIONS = 'Scores';
 
 export class DatabaseService {
-    private db: Db;
+    private HighScore_DB: Db;
     private client: MongoClient;
     // private options: MongoClientOptions = {
     //     useNewUrlParser: true,
     //     useUnifiedTopology: true,
     // };
 
-    async start(url: string = DATABASE_URL): Promise<MongoClient | null> {
+    async start(url: string): Promise<MongoClient | null> {
         try {
             const client = await MongoClient.connect(url);
             this.client = client;
-            this.db = client.db(DATABASE_NAME);
+            this.HighScore_DB = client.db(DATABASE.highScore.name);
         } catch {
             throw Error('Database connection error');
-        }
-
-        if ((await this.db.collection(DATABASE_COLLECTIONS).countDocuments()) === 0) {
-            await this.populateDB();
         }
         return this.client;
     }
@@ -31,41 +28,10 @@ export class DatabaseService {
         return this.client.close();
     }
 
-    async populateDB(): Promise<void> {
-        const Scores: Scores[] = [
-            {
-                id: 0,
-                name: 'DefaultUser0',
-                score: 0,
-            },
-            {
-                id: 1,
-                name: 'DefaultUser1',
-                score: 0,
-            },
-            {
-                id: 2,
-                name: 'DefaultUser2',
-                score: 0,
-            },
-            {
-                id: 3,
-                name: 'DefaultUser3',
-                score: 0,
-            },
-            {
-                id: 4,
-                name: 'DefaultUser4',
-                score: 0,
-            },
-        ];
-
-        for (const Score of Scores) {
-            await this.db.collection(DATABASE_COLLECTIONS).insertOne(Score);
-        }
-    }
-
-    get database(): Db {
-        return this.db;
+    async resetDB() {
+        return Promise.all([
+            this.HighScore_DB.dropCollection(DATABASE.highScore.collections.classical),
+            this.HighScore_DB.dropCollection(DATABASE.highScore.collections.log2290),
+        ]);
     }
 }
