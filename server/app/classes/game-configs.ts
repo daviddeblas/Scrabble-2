c45fd962c77d84bf52efab83c7f294d223f20ac2
mymanat <71021952+mymanat@users.noreply.github.com>
2022-03-09 21:47:52 -0500

    ajouter un attribut mode de jeu

--- server/app/classes/game-configs.ts
+++ server/app/classes/game-configs.ts
@@ -1,5 +1,12 @@
 import { GameConfig } from '@app/classes/game-config';
 
+export enum GameMode {
+    None,
+    Classical,
+    Log2990,
+}
+
 export class GameConfigs {
     configs: GameConfig[];
+    gameMode: number;
 }
