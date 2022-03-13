8420cb39f9cc396cb34b51c89a1ffece4c80bed4
nico <23260358+nico14916@users.noreply.github.com>
2022-03-13 17:26:34 -0400

    solver: division ligne par segments

--- server/app/classes/solver.ts
+++ server/app/classes/solver.ts
@@ -1,5 +1,33 @@
 import { Letter } from 'common/classes/letter';
 import { Dictionary } from './dictionary';
+
+interface Segment {
+    value: string;
+    start: number;
+    end: number;
+}
+
 export class Solver {
     constructor(private dictionary: Dictionary, private board: (Letter | null)[][], private easel: Letter[]) {}
+
+    generateSegments(line: (Letter | null)[]): Segment[] {
+        const segments: Segment[] = [];
+
+        let pos = 0;
+        while (pos < line.length) {
+            while (line[pos] === null && pos < line.length) pos++;
+            if (pos === line.length) break;
+
+            const segment: Segment = { start: pos, value: '', end: pos };
+            while (line[pos] !== null && pos < line.length) {
+                segment.value += line[pos];
+                pos++;
+            }
+
+            segment.end = pos;
+            segments.push(segment);
+        }
+
+        return segments;
+    }
 }
