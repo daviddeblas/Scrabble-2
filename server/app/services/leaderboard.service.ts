4490aa78b4bb844070e8d51b3f0976743e8d76eb
mymanat <71021952+mymanat@users.noreply.github.com>
2022-03-14 10:37:13 -0400

    bugg sur la fonction getHighScores reglee

--- server/app/services/leaderboard.service.ts
+++ server/app/services/leaderboard.service.ts
@@ -1,34 +1,34 @@
 import { GameMode } from '@app/classes/game-configs';
 import { DATABASE, DEFAULT_HIGHSCORE, HighScore } from '@app/classes/highscore';
-import { Db, Document } from 'mongodb';
+import { Db, Document, WithId } from 'mongodb';
 
 export class LeaderboardService {
-    private highScore_DB: Db;
+    private highScoreDB: Db;
 
-    // async getHighscores(gameMode: number): Promise<HighScore[]> {
-    //     return this.highScore_DB
-    //         .collection(DATABASE.highScore.collections[gameMode === GameMode.Classical ? 'classical' : 'log2990'])
-    //         .find({})
-    //         .sort({ score: -1 })
-    //         .toArray()
-    //         .then((score: HighScore[]) => {
-    //             return score;
-    //         });
-    // }
+    async getHighscores(gameMode: number): Promise<HighScore[]> {
+        return this.highScoreDB
+            .collection(DATABASE.highScore.collections[gameMode === GameMode.Classical ? 'classical' : 'log2990'])
+            .find({})
+            .sort({ score: -1 })
+            .toArray()
+            .then((score: WithId<Document>[]) => {
+                return score.map((s) => s as unknown as HighScore);
+            });
+    }
 
     async addDefaultScores(): Promise<void> {
         await Promise.all([
-            this.highScore_DB.collection(DATABASE.highScore.collections.classical).insertMany(DEFAULT_HIGHSCORE.classical),
-            this.highScore_DB.collection(DATABASE.highScore.collections.log2290).insertMany(DEFAULT_HIGHSCORE.log2990),
+            this.highScoreDB.collection(DATABASE.highScore.collections.classical).insertMany(DEFAULT_HIGHSCORE.classical),
+            this.highScoreDB.collection(DATABASE.highScore.collections.log2290).insertMany(DEFAULT_HIGHSCORE.log2990),
         ]);
     }
 
     async updateHighScore(highScore: HighScore, gameMode: number): Promise<void> {
         const collection = DATABASE.highScore.collections[gameMode === GameMode.Classical ? 'classical' : 'log2990'];
-        const lowestScore = await this.highScore_DB.collection(collection).find({}).sort({ score: 1 }).limit(1).toArray();
+        const lowestScore = await this.highScoreDB.collection(collection).find({}).sort({ score: 1 }).limit(1).toArray();
         if (highScore.score > lowestScore[0].score) {
-            this.highScore_DB.collection(collection).insertOne(highScore);
-            this.highScore_DB.collection(collection).deleteOne({ id: (lowestScore[0] as Document).id });
+            this.highScoreDB.collection(collection).insertOne(highScore);
+            this.highScoreDB.collection(collection).deleteOne({ id: (lowestScore[0] as Document).id });
         }
     }
 }
