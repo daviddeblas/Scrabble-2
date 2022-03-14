cca1ed71fed1bb84530f82cc159f530a6913601e
mymanat <71021952+mymanat@users.noreply.github.com>
2022-03-14 10:38:54 -0400

    debut des tests pour se connecter à la DB

--- server/app/services/database.service.spec.ts
+++ server/app/services/database.service.spec.ts
@@ -1,23 +1,37 @@
 import { DatabaseService } from '@app/services/database.service';
+import { expect } from 'chai';
+import { describe } from 'mocha';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 
-describe('Database service', ()=>{
+describe('Database service', () => {
     let databaseService: DatabaseService;
     let mongoServer: MongoMemoryServer;
-    const player1= {name: 'player1' score:100};
-    const player2= {name: 'player2' score:10};
+    const player1 = { name: 'player1', score: 100 };
+    const player2 = { name: 'player2', score: 10 };
 
-    beforeEach(async()=>{
-        databaseService= new DatabaseService();
-
-        mongoServer=await MongoMemoryServer.create();
-        
+    beforeEach(async () => {
+        databaseService = new DatabaseService();
+        mongoServer = await MongoMemoryServer.create();
     });
 
-    afterEach(async()=>{
-        if(databaseService['client']){
-            await databaseService['client'].close();
+    afterEach(async () => {
+        if (databaseService.client) {
+            await databaseService.client.close();
         }
     });
 
-})
+    it('should connect to the database when databaseConnect is called', async () => {
+        const mongoUri = await mongoServer.getUri();
+        await databaseService.DatabaseConnect(mongoUri);
+        expect(databaseService.client).toBeDefined();
+    });
+
+    it('should not connect to the database when databaseConnect is called with the wrong URL', async () => {
+        try {
+            await databaseService.databaseConnect('WRONG URL');
+            fail();
+        } catch {
+            expect(databaseService.client).not.toBeDefined();
+        }
+    });
+});
