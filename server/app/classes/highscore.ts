5d2fcd0d5aeb0ecaccde02deb35089249927d466
mymanat <71021952+mymanat@users.noreply.github.com>
2022-03-17 10:46:17 -0400

    rectifier les scores par defaut

--- server/app/classes/highscore.ts
+++ server/app/classes/highscore.ts
@@ -6,17 +6,17 @@ export interface HighScore {
 export const DEFAULT_HIGHSCORE = {
     classical: [
         { name: 'name1', score: 0 },
-        { name: 'name2', score: 0 },
-        { name: 'name3', score: 0 },
-        { name: 'name4', score: 0 },
-        { name: 'name5', score: 0 },
+        { name: 'name2', score: 1 },
+        { name: 'name3', score: 2 },
+        { name: 'name4', score: 3 },
+        { name: 'name5', score: 4 },
     ] as HighScore[],
     log2990: [
         { name: 'name1', score: 0 },
-        { name: 'name2', score: 0 },
-        { name: 'name3', score: 0 },
-        { name: 'name4', score: 0 },
-        { name: 'name5', score: 0 },
+        { name: 'name2', score: 1 },
+        { name: 'name3', score: 2 },
+        { name: 'name4', score: 3 },
+        { name: 'name5', score: 4 },
     ] as HighScore[],
 };
 
