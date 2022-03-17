3202012773fc1b8bf110cabe6d0af6222fd8e1b1
Raphael Tremblay <raphaeltremblay01@gmail.com>
2022-03-16 22:40:01 -0400

    Ajouts de tests pour les classes pour la fonctionnalite placer lettre souris

---
+++ client/src/app/classes/board-selection.spec.ts
@@ -0,0 +1,27 @@
+import { Vec2 } from 'common/classes/vec2';
+import { BoardSelection, Orientation } from './board-selection';
+
+describe('BoardSelection', () => {
+    it('should create an instance', () => {
+        const boardSelection = new BoardSelection();
+        expect(boardSelection).toBeTruthy();
+        expect(boardSelection.cell).toBeNull();
+        expect(boardSelection.modifiedCells).toEqual([]);
+        expect(boardSelection.orientation).toBeNull();
+    });
+
+    it('should create a clone of the selection', () => {
+        const boardSelection = new BoardSelection(new Vec2(1, 1), Orientation.Horizontal, [new Vec2(0, 1)]);
+        let cloneSelection = boardSelection.copy();
+
+        expect(cloneSelection.cell).not.toBe(boardSelection.cell);
+        expect(cloneSelection.cell).toEqual(boardSelection.cell);
+        expect(cloneSelection.modifiedCells).not.toBe(boardSelection.modifiedCells);
+        expect(cloneSelection.modifiedCells).toEqual(boardSelection.modifiedCells);
+        expect(cloneSelection.orientation).toEqual(boardSelection.orientation);
+
+        boardSelection.cell = null;
+        cloneSelection = boardSelection.copy();
+        expect(cloneSelection.cell).toBeNull();
+    });
+});
