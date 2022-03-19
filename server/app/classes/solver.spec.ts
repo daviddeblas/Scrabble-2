acbfa41699f40ac0a0f4f41d586c794fe94a5746
nico <23260358+nico14916@users.noreply.github.com>
2022-03-19 16:05:45 -0400

    solver: test filtre des mots perpendiculaire

--- server/app/classes/solver.spec.ts
+++ server/app/classes/solver.spec.ts
@@ -1,11 +1,14 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
 import dictionaryJson from '@app/../assets/dictionary.json';
 import { Dictionary } from '@app/classes/dictionary';
 import { expect } from 'chai';
+import { Vec2 } from 'common/classes/vec2';
 import { BOARD_SIZE } from 'common/constants';
 import { Board } from './game/board';
-import { Solver } from './solver';
+import { PlacedLetter } from './placed-letter';
+import { Solution, Solver } from './solver';
 
-describe('solver', () => {
+describe.only('solver', () => {
     const dictionary: Dictionary = Object.assign(new Dictionary(), dictionaryJson);
     let board: Board;
     beforeEach(() => {
@@ -83,5 +86,36 @@ describe('solver', () => {
                 index: 2,
             },
         ]);
+    });
+
+    it('should filter invalid affected words', () => {
+        const dictionarySample = { words: ['aaa', 'bbb'] } as Dictionary;
+        // prettier-ignore
+        {
+            board.board[3] = [null, null, null, null, null, null,  'A', null, null,  'C', null, null, null, null, null];
+            board.board[4] = [null, null, null, null, null, null,  'A', null, null,  'C', null, null, null, null, null];
+            board.board[5] = [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null];
+            board.board[6] = [null, null, null, null, null, null, null, null,  'B', null, null, null, null, null, null];
+            board.board[7] = [null, null, null, null, null, null, null, null,  'B', null, null, null, null, null, null];
+        }
+        const solver: Solver = new Solver(dictionarySample, board, []);
+        const solutions = solver.filterInvalidAffectedWords(
+            [
+                { letters: [null, null, null, null, null, null, 'A', null, 'B', 'C', null, null, null, null, null], blanks: [] },
+                { letters: [null, null, null, null, null, null, 'A', null, 'B', null, null, null, null, null, null], blanks: [8] },
+            ],
+            new Vec2(0, 1),
+            new Vec2(5, 0),
+        );
+
+        const expected: Solution[] = [
+            {
+                letters: [new PlacedLetter('A', new Vec2(5, 6)), new PlacedLetter('B', new Vec2(5, 8))],
+                blanks: [new Vec2(5, 8)],
+                direction: new Vec2(0, 1),
+            },
+        ];
+
+        expect(solutions).to.deep.equal(expected);
     });
 });
