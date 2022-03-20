111196eaeb6434c0f65fac090ae4259bb2a41d8a
mymanat <71021952+mymanat@users.noreply.github.com>
2022-03-20 10:52:55 -0400

    enlever les promesse des fonctions

--- server/app/services/database.service.ts
+++ server/app/services/database.service.ts
@@ -1,5 +1,5 @@
 import { DATABASE, DEFAULT_HIGHSCORE, HighScore } from '@app/classes/highscore';
-import { Db, MongoClient, MongoClientOptions } from 'mongodb';
+import { Db, MongoClient } from 'mongodb';
 import { Service } from 'typedi';
 
 @Service()
@@ -7,32 +7,39 @@ export class DatabaseService {
     private highScoreDB: Db;
     private client: MongoClient;
 
-    private options: MongoClientOptions = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    };
-
-    async start(url: string): Promise<MongoClient | null> {
+    async start() {
         try {
-            const client = await MongoClient.connect(url, this.options);
+            const client = new MongoClient(DATABASE.uri);
             this.client = client;
             this.highScoreDB = client.db(DATABASE.highScore.name);
         } catch {
             throw Error('Database connection error');
         }
+        await this.client.connect();
+
         if ((await this.highScoreDB.collection(DATABASE.highScore.collections.classical).countDocuments()) === 0) {
-            await this.populateDB();
+            await this.populateDBClassical();
         }
 
-        return this.client;
+        if ((await this.highScoreDB.collection(DATABASE.highScore.collections.log2290).countDocuments()) === 0) {
+            await this.populateDBlog2990();
+        }
     }
 
     async closeConnection(): Promise<void> {
         return this.client.close();
     }
 
-    async populateDB(): Promise<void> {
+    async populateDBClassical(): Promise<void> {
         const scores: HighScore[] = DEFAULT_HIGHSCORE.classical;
+
+        for (const score of scores) {
+            await this.highScoreDB.collection(DATABASE.highScore.collections.classical).insertOne(score);
+        }
+    }
+
+    async populateDBlog2990(): Promise<void> {
+        const scores: HighScore[] = DEFAULT_HIGHSCORE.log2990;
 
         for (const score of scores) {
             await this.highScoreDB.collection(DATABASE.highScore.collections.classical).insertOne(score);
