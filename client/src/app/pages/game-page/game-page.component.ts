c79d11b015229698604e678ce56646449643342a
DavidDeBlas <daviddeblas@ymail.com>
2022-03-24 00:09:51 -0400

    création du front-end montrant les objectifs privés et publics

--- client/src/app/pages/game-page/game-page.component.ts
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { browserReload } from '@app/actions/browser.actions';
 import { getGameStatus } from '@app/actions/game-status.actions';
+import { ObjectiveManagerService } from '@app/services/objective-manager.service';
 import { Store } from '@ngrx/store';
 
 @Component({
@@ -8,10 +9,15 @@ import { Store } from '@ngrx/store';
     templateUrl: './game-page.component.html',
     styleUrls: ['./game-page.component.scss'],
 })
-export class GamePageComponent {
-    constructor(private store: Store) {
+export class GamePageComponent implements OnInit {
+    isLog2990: boolean = true;
+    constructor(private store: Store, private objectiveService: ObjectiveManagerService) {
         store.dispatch(getGameStatus());
         window.addEventListener('load', (event) => this.catchBrowserLoad(event));
+    }
+
+    ngOnInit() {
+        this.objectiveService.mode.subscribe((message) => (this.isLog2990 = message));
     }
 
     catchBrowserLoad(event: Event) {
