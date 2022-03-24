29b4f845b656a0826eb206845a2a9609a8fa710b
DavidDeBlas <daviddeblas@ymail.com>
2022-03-24 00:15:12 -0400

    possibilité de lancer la partie dans le mode LOG2990 (seulement front-end disponible)

--- client/src/app/pages/main-page/main-page.component.ts
+++ client/src/app/pages/main-page/main-page.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ObjectiveManagerService } from '@app/services/objective-manager.service';
 
 @Component({
     selector: 'app-main-page',
     templateUrl: './main-page.component.html',
     styleUrls: ['./main-page.component.scss'],
 })
-export class MainPageComponent {}
+export class MainPageComponent implements OnInit {
+    mode2990Chosen: boolean = false;
+
+    constructor(private objectiveService: ObjectiveManagerService) {}
+
+    ngOnInit() {
+        this.objectiveService.mode.subscribe((message) => (this.mode2990Chosen = message));
+    }
+
+    chooseMode2990() {
+        this.mode2990Chosen = true;
+        this.objectiveService.nextMessage(this.mode2990Chosen);
+    }
+}
