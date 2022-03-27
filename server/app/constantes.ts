91c32f1560f459971adc02028dd94d14d13022e7
theolouesdon <65035689+theolouesdon@users.noreply.github.com>
2022-03-27 15:08:49 -0400

    Amélioration du descriptif des objectifs

--- server/app/constantes.ts
+++ server/app/constantes.ts
@@ -8,37 +8,37 @@ export const LOG2990OBJECTIVES: Log2990Objective[] = [
     },
     {
         description: 'Placer 3 ou plus consonnes seulement',
-        value: 'Bonus de 60',
+        value: 'Bonus de 60 points',
         isValidated: false,
     },
     {
         description: "Ralonger le début et la fin d'un mot existant de plus de deux lettres",
-        value: 'Bonus de 30',
+        value: 'Bonus de 30 points',
         isValidated: false,
     },
     {
         description: 'Faire un placement rapportant plus de 20 points dans les 10 premières secondes du tour',
-        value: 'Bonus de 45',
+        value: 'Bonus de 45 points',
         isValidated: false,
     },
     {
         description: 'Placer deux lettres qui valent 8 points ou plus en un tour',
-        value: 'Bonus de 70',
+        value: 'Bonus de 70 points',
         isValidated: false,
     },
     {
         description: 'Faire un mot de plus de 10 lettres',
-        value: 'Bonus de 50',
+        value: 'Bonus de 50 points',
         isValidated: false,
     },
     {
         description: 'Avoir exactement 69 points',
-        value: 'Bonus de 69',
+        value: 'Bonus de 69 points',
         isValidated: false,
     },
     {
         description: 'Placer le mot : ',
-        value: 'Bonus de 70',
+        value: 'Bonus de 70 points',
         isValidated: false,
     },
 ];
