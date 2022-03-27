0df1210527bb7945ea8b2fbcf017218f71e312bc
theolouesdon <65035689+theolouesdon@users.noreply.github.com>
2022-03-27 11:09:57 -0400

    Tests coté client

--- client/src/app/components/games-objectives/games-objectives.component.spec.ts
+++ client/src/app/components/games-objectives/games-objectives.component.spec.ts
@@ -1,20 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppMaterialModule } from '@app/modules/material.module';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { GamesObjectivesComponent } from './games-objectives.component';
 
 describe('GamesObjectivesComponent', () => {
     let component: GamesObjectivesComponent;
     let fixture: ComponentFixture<GamesObjectivesComponent>;
+    let store: MockStore;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
+            imports: [BrowserAnimationsModule, AppMaterialModule],
             declarations: [GamesObjectivesComponent],
-            imports: [AppMaterialModule, BrowserAnimationsModule],
+            providers: [provideMockStore()],
         }).compileComponents();
     });
 
     beforeEach(() => {
+        store = TestBed.inject(MockStore);
+        store.overrideSelector('gameObjective', { publicObjectives: [], privateObjectives: [] });
         fixture = TestBed.createComponent(GamesObjectivesComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
