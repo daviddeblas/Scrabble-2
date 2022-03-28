bb8cda2f61dc9797050d0cfb02a3c77e2e05a036
theolouesdon <65035689+theolouesdon@users.noreply.github.com>
2022-03-28 15:02:48 -0400

    Ajout du nom du mode de jeu dans la selection page

--- client/src/app/pages/game-selection-page/game-selection-page.component.ts
+++ client/src/app/pages/game-selection-page/game-selection-page.component.ts
@@ -3,6 +3,9 @@ import { MatDialog } from '@angular/material/dialog';
 import { GameJoinPageComponent } from '@app/pages/game-join-page/game-join-page.component';
 import { GamePreparationPageComponent } from '@app/pages/game-preparation-page/game-preparation-page.component';
 import { SoloGameSettingsPageComponent } from '@app/pages/solo-game-settings-page/solo-game-settings-page.component';
+import { Store } from '@ngrx/store';
+import { GameMode } from 'common/interfaces/game-mode';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-game-selection-page',
@@ -10,7 +13,10 @@ import { SoloGameSettingsPageComponent } from '@app/pages/solo-game-settings-pag
     styleUrls: ['./game-selection-page.component.scss'],
 })
 export class GameSelectionPageComponent {
-    constructor(public dialog: MatDialog) {}
+    gameMode$: Observable<GameMode>;
+    constructor(public dialog: MatDialog, store: Store<{ gameMode: GameMode }>) {
+        this.gameMode$ = store.select('gameMode');
+    }
     openSoloSettingsPage(): void {
         this.dialog.open(SoloGameSettingsPageComponent);
     }
