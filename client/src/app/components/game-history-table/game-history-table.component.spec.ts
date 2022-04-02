d4b284e25fea467bfd1e10a12aff1bb7deb4ed49
DavidDeBlas <daviddeblas@ymail.com>
2022-04-02 19:56:25 -0400

    modification du component game-history-table pour qu'il réagisse en temps réel au changement

--- client/src/app/components/game-history-table/game-history-table.component.spec.ts
+++ client/src/app/components/game-history-table/game-history-table.component.spec.ts
@@ -1,25 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { AppMaterialModule } from '@app/modules/material.module';
 import { GameHistoryTableComponent } from './game-history-table.component';
 
 describe('GameHistoryTableComponent', () => {
-  let component: GameHistoryTableComponent;
-  let fixture: ComponentFixture<GameHistoryTableComponent>;
+    let component: GameHistoryTableComponent;
+    let fixture: ComponentFixture<GameHistoryTableComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ GameHistoryTableComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [GameHistoryTableComponent],
+            imports: [AppMaterialModule],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(GameHistoryTableComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(GameHistoryTableComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
