ec71a6163e08ae22168fb6c4161cc7ae515a3aa3
Raphael Tremblay <raphaeltremblay01@gmail.com>
2022-04-04 00:34:33 -0400

    Construction de la structure de la page

--- client/src/app/pages/admin-page/admin-page.component.ts
+++ client/src/app/pages/admin-page/admin-page.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-admin-page',
-  templateUrl: './admin-page.component.html',
-  styleUrls: ['./admin-page.component.scss']
+    selector: 'app-admin-page',
+    templateUrl: './admin-page.component.html',
+    styleUrls: ['./admin-page.component.scss'],
 })
 export class AdminPageComponent implements OnInit {
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
