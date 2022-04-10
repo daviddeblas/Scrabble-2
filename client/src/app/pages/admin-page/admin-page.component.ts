cdd8f8e77faceab6a9bb937189341077c263f2b6
Raphael Tremblay <raphaeltremblay01@gmail.com>
2022-04-09 22:10:11 -0400

    Perfection du UI et fix avec le nouveau reducer dictionnaire

--- client/src/app/pages/admin-page/admin-page.component.ts
+++ client/src/app/pages/admin-page/admin-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { loadDictionaries } from '@app/actions/dictionaries.actions';
 import { Store } from '@ngrx/store';
+import { iDictionary } from 'common/interfaces/dictionary';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -9,9 +10,9 @@ import { Observable } from 'rxjs';
     styleUrls: ['./admin-page.component.scss'],
 })
 export class AdminPageComponent implements OnInit {
-    dictionaries$: Observable<string[]>;
+    dictionaries$: Observable<iDictionary[]>;
 
-    constructor(store: Store<{ dictionaries: string[] }>) {
+    constructor(store: Store<{ dictionaries: iDictionary[] }>) {
         this.dictionaries$ = store.select('dictionaries');
         store.dispatch(loadDictionaries());
     }
