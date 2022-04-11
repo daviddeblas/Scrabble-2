2d6d60270a637347562c6b96ea1866fb9299e1c5
Raphael Tremblay <raphaeltremblay01@gmail.com>
2022-04-11 10:58:22 -0400

    Bon, eslint pas content

--- client/src/app/components/dictionaries-administrator/dictionaries-administrator.component.ts
+++ client/src/app/components/dictionaries-administrator/dictionaries-administrator.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { deleteDictionary, downloadDictionary, loadDictionaries, resetDictionaries } from '@app/actions/dictionaries.actions';
+import { DictionaryFormDialogComponent } from '@app/components/dictionary-form-dialog/dictionary-form-dialog.component';
 import { Store } from '@ngrx/store';
 import { iDictionary } from 'common/interfaces/dictionary';
 import { Observable } from 'rxjs';
-import { DictionaryFormDialogComponent } from '../dictionary-form-dialog/dictionary-form-dialog.component';
 
 @Component({
     selector: 'app-dictionaries-administrator',
     templateUrl: './dictionaries-administrator.component.html',
     styleUrls: ['./dictionaries-administrator.component.scss'],
 })
-export class DictionariesAdministratorComponent implements OnInit {
+export class DictionariesAdministratorComponent {
     dictionaries$: Observable<iDictionary[]>;
 
     constructor(private store: Store<{ dictionaries: iDictionary[] }>, public dialog: MatDialog) {
@@ -43,6 +43,4 @@ export class DictionariesAdministratorComponent implements OnInit {
     downloadDictionary(dictionary: iDictionary) {
         this.store.dispatch(downloadDictionary({ dictionary }));
     }
-
-    ngOnInit(): void {}
 }
