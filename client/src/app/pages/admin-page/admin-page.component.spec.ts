9d5caa9c40c7d5d74e54d7291d13470274ea2e9a
theolouesdon <65035689+theolouesdon@users.noreply.github.com>
2022-04-11 15:08:47 -0400

    Tests pour le Game History service

--- client/src/app/pages/admin-page/admin-page.component.spec.ts
+++ client/src/app/pages/admin-page/admin-page.component.spec.ts
@@ -1,14 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { resetGameHistory } from '@app/actions/game-history.actions';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { cold } from 'jasmine-marbles';
 import { AdminPageComponent } from './admin-page.component';
 
 describe('AdminPageComponent', () => {
     let component: AdminPageComponent;
     let fixture: ComponentFixture<AdminPageComponent>;
+    let store: MockStore;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [AdminPageComponent],
+            providers: [provideMockStore()],
         }).compileComponents();
+        store = TestBed.inject(MockStore);
     });
 
     beforeEach(() => {
@@ -19,5 +25,11 @@ describe('AdminPageComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should dispatch resetGameHistory when resetGameHistory called', () => {
+        component.resetGameHistory();
+        const expectedAction = cold('a', { a: resetGameHistory() });
+        expect(store.scannedActions$).toBeObservable(expectedAction);
     });
 });
