84981a501114940c366d97bcfdc11f1e8a5fd951
theolouesdon <65035689+theolouesdon@users.noreply.github.com>
2022-04-12 10:18:20 -0400

    Fix des erreurs de test

--- client/src/app/components/dictionary-form-dialog/dictionary-form-dialog.component.spec.ts
+++ client/src/app/components/dictionary-form-dialog/dictionary-form-dialog.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from '@app/modules/material.module';
 import { provideMockStore } from '@ngrx/store/testing';
 import { DictionaryFormDialogComponent } from './dictionary-form-dialog.component';
 
@@ -10,7 +12,7 @@ describe('DictionaryFormDialogComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [ReactiveFormsModule, FormsModule],
+            imports: [ReactiveFormsModule, FormsModule, AppMaterialModule, BrowserAnimationsModule],
             declarations: [DictionaryFormDialogComponent],
             providers: [
                 provideMockStore(),
