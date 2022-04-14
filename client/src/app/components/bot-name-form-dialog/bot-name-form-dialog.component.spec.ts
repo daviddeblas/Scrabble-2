054a83d67bdb574bda3a11a2e2091c458ea26946
theolouesdon <65035689+theolouesdon@users.noreply.github.com>
2022-04-14 09:30:11 -0400

    Ajout d'un message d'erreur si un nom invalide est entrée

--- client/src/app/components/bot-name-form-dialog/bot-name-form-dialog.component.spec.ts
+++ client/src/app/components/bot-name-form-dialog/bot-name-form-dialog.component.spec.ts
@@ -1,6 +1,9 @@
+/* eslint-disable dot-notation */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { addBotName, modifyBotName } from '@app/actions/bot-names.actions';
 import { AppMaterialModule } from '@app/modules/material.module';
@@ -13,6 +16,10 @@ describe('BotNameFormDialogComponent', () => {
         close: jasmine.createSpy('close'),
     };
 
+    const mockSnackBarSpy: { open: jasmine.Spy } = {
+        open: jasmine.createSpy('open'),
+    };
+
     let component: BotNameFormDialogComponent;
     let fixture: ComponentFixture<BotNameFormDialogComponent>;
     let store: MockStore;
@@ -23,7 +30,14 @@ describe('BotNameFormDialogComponent', () => {
             declarations: [BotNameFormDialogComponent],
             providers: [
                 FormBuilder,
-                provideMockStore(),
+                provideMockStore({
+                    selectors: [
+                        {
+                            selector: 'botNames',
+                            value: { easy: ['ROB'], hard: ['MIKE'] },
+                        },
+                    ],
+                }),
                 {
                     provide: MatDialogRef,
                     useValue: {},
@@ -31,6 +45,10 @@ describe('BotNameFormDialogComponent', () => {
                 {
                     provide: MatDialogRef,
                     useValue: mockDialogSpy,
+                },
+                {
+                    provide: MatSnackBar,
+                    useValue: mockSnackBarSpy,
                 },
             ],
         }).compileComponents();
@@ -82,11 +100,38 @@ describe('BotNameFormDialogComponent', () => {
 
         component.settingsForm.controls.name.setValue(name);
         component.currentDifficulty = difficulty;
-        fixture.detectChanges();
         component.oldName = oldName;
         const expectedAction = cold('a', { a: modifyBotName({ oldName, newName: name, difficulty }) });
         component.onSubmit();
         expect(store.scannedActions$).toBeObservable(expectedAction);
         expect(mockDialogSpy.close).toHaveBeenCalled();
+    });
+
+    it('onSubmit should call dispatchError if verifyNameValidity return true', () => {
+        spyOn(component as any, 'verifyNameValidity').and.callFake(() => true);
+        const dispatchErrorSpy = spyOn(component as any, 'dispatchError');
+        component.onSubmit();
+        expect(dispatchErrorSpy).toHaveBeenCalled();
+    });
+
+    it('verifyNameValidity should return false if name not in easy or hard bot names', () => {
+        const name = 'randomName';
+        expect(component['verifyNameValidity'](name)).toBeFalse();
+    });
+
+    it('verifyNameValidity should return true if name in easy bot names', () => {
+        const name = 'ROB';
+        expect(component['verifyNameValidity'](name)).toBeTrue();
+    });
+
+    it('verifyNameValidity should return true if name in hard bot names', () => {
+        const name = 'MIKE';
+        expect(component['verifyNameValidity'](name)).toBeTrue();
+    });
+
+    it('dispatchError should call open from snackBar', () => {
+        const message = 'Message';
+        component['dispatchError'](message);
+        expect(mockSnackBarSpy.open).toHaveBeenCalled();
     });
 });
--- client/src/app/components/bot-name-form-dialog/bot-name-form-dialog.component.ts
+++ client/src/app/components/bot-name-form-dialog/bot-name-form-dialog.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 import { addBotName, modifyBotName } from '@app/actions/bot-names.actions';
 import { MAX_NAME_LENGTH, MIN_NAME_LENGTH } from '@app/constants';
+import { BotNames } from '@app/interfaces/bot-names';
 import { Store } from '@ngrx/store';
 
 @Component({
@@ -15,7 +17,12 @@ export class BotNameFormDialogComponent implements OnInit {
     currentDifficulty: string;
     oldName: string | undefined;
     settingsForm: FormGroup;
-    constructor(private fb: FormBuilder, private store: Store, private dialogRef: MatDialogRef<BotNameFormDialogComponent>) {}
+    constructor(
+        private fb: FormBuilder,
+        private store: Store<{ botNames: BotNames }>,
+        private dialogRef: MatDialogRef<BotNameFormDialogComponent>,
+        private snackBar: MatSnackBar,
+    ) {}
 
     ngOnInit(): void {
         this.settingsForm = this.fb.group({
@@ -28,12 +35,32 @@ export class BotNameFormDialogComponent implements OnInit {
     }
 
     onSubmit(): void {
-        if (!this.oldName)
-            this.store.dispatch(addBotName({ name: this.settingsForm.controls.name.value, difficulty: this.settingsForm.controls.difficulty.value }));
+        const chosenName = this.settingsForm.controls.name.value;
+        if (this.verifyNameValidity(chosenName)) {
+            this.dispatchError('Nom déjà présent dans les listes');
+            return;
+        }
+        if (!this.oldName) this.store.dispatch(addBotName({ name: chosenName, difficulty: this.settingsForm.controls.difficulty.value }));
         else
             this.store.dispatch(
                 modifyBotName({ oldName: this.oldName, newName: this.settingsForm.controls.name.value, difficulty: this.currentDifficulty }),
             );
         this.dialogRef.close();
+    }
+
+    private verifyNameValidity(name: string): boolean {
+        let nameIsNotValid = false;
+        this.store.select('botNames').subscribe((names) => {
+            nameIsNotValid ||= names.easy.includes(name);
+            nameIsNotValid ||= names.hard.includes(name);
+        });
+        return nameIsNotValid;
+    }
+
+    private dispatchError(message: string): void {
+        this.settingsForm.controls.name.setValue(this.oldName);
+        const durationMilliseconds = 3000;
+        const configuration: MatSnackBarConfig = { duration: durationMilliseconds };
+        this.snackBar.open(message, 'Compris', configuration);
     }
 }
