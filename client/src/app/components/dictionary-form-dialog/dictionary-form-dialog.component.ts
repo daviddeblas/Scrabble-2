69c4dd40762e8f9ec957a23c9346d187b5d5e2aa
theolouesdon <65035689+theolouesdon@users.noreply.github.com>
2022-04-14 10:21:30 -0400

    Ajout de verification pour extention de fichier

--- client/src/app/components/dictionary-form-dialog/dictionary-form-dialog.component.ts
+++ client/src/app/components/dictionary-form-dialog/dictionary-form-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 import { addDictionary, modifyDictionary } from '@app/actions/dictionaries.actions';
 import { Store } from '@ngrx/store';
 import { iDictionary } from 'common/interfaces/dictionary';
@@ -19,7 +20,12 @@ export class DictionaryFormDialogComponent implements OnInit {
     loadedDictionary: iDictionary | undefined;
     loadedFile: File;
 
-    constructor(private fb: FormBuilder, private store: Store, private dialogRef: MatDialogRef<DictionaryFormDialogComponent>) {}
+    constructor(
+        private fb: FormBuilder,
+        private store: Store,
+        private dialogRef: MatDialogRef<DictionaryFormDialogComponent>,
+        private snackBar: MatSnackBar,
+    ) {}
 
     ngOnInit(): void {
         this.settingsForm = this.fb.group({
@@ -30,15 +36,21 @@ export class DictionaryFormDialogComponent implements OnInit {
     }
 
     onFileSelected(event: Event) {
-        // Ca au moins ca compile
         this.loadedFile = (event as unknown as { target: { files: File[] } }).target.files[0];
-
+        const fileExtension = this.loadedFile.name.split('.').pop();
+        if (fileExtension !== 'json') {
+            this.dispatchFileError('Mauvaise extention de fichier');
+            return;
+        }
         this.loadedFile.text().then((text) => {
-            this.loadedDictionary = JSON.parse(text);
-            if (this.loadedDictionary) {
-                this.settingsForm.controls.title.setValue(this.loadedDictionary.title);
-                this.settingsForm.controls.description.setValue(this.loadedDictionary.description);
+            const jsonFile = JSON.parse(text);
+            if (jsonFile.title === undefined || jsonFile.description === undefined || jsonFile.words === undefined) {
+                this.dispatchFileError('Mauvais attributs dans le fichier json');
+                return;
             }
+            this.loadedDictionary = jsonFile;
+            this.settingsForm.controls.title.setValue(jsonFile.title);
+            this.settingsForm.controls.description.setValue(jsonFile.description);
             this.settingsForm.controls.title.enable();
             this.settingsForm.controls.description.enable();
         });
@@ -64,5 +76,14 @@ export class DictionaryFormDialogComponent implements OnInit {
 
     private getFormDictionary(): iDictionary {
         return { title: this.settingsForm.controls.title.value, description: this.settingsForm.controls.description.value };
+    }
+
+    private dispatchFileError(message: string): void {
+        this.settingsForm.controls.file.setValue('');
+        this.settingsForm.controls.title.setValue('');
+        this.settingsForm.controls.description.setValue('');
+        const durationMilliseconds = 3000;
+        const configuration: MatSnackBarConfig = { duration: durationMilliseconds };
+        this.snackBar.open(message, 'Compris', configuration);
     }
 }
