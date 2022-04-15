8ea8e7e1832d34d8071490703479f0ec56bd0ed3
theolouesdon <65035689+theolouesdon@users.noreply.github.com>
2022-04-15 19:27:50 -0400

    Ajout de justification pour eslint disable

--- client/src/app/effects/bot-names.effects.ts
+++ client/src/app/effects/bot-names.effects.ts
@@ -1,4 +1,8 @@
 /* eslint-disable no-invalid-this */
+// Syntaxe utilisé sur le site de ngRx
+// Necessaire pour utiliser les actions dans les fichiers .effects, si on enleve la ligne esLint: unexpected this
+// Si on enleve le esLint : erreur de TypeScript
+
 import { Injectable } from '@angular/core';
 import { addBotName, deleteBotName, loadBotNames, modifyBotName, resetBotNames } from '@app/actions/bot-names.actions';
 import { BotNamesService } from '@app/services/bot-names.service';
